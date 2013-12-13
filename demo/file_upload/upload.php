f3f0612001810e6f2e2a740180163e216bbeef0c
yang yifeng <yyf44@qq.com>
2013-12-13 15:40:07 +0800

    edit

--- demo/file_upload/upload.php
+++ demo/file_upload/upload.php
@@ -1,7 +1,10 @@
 <?php
 
-    $file = fopen( $_POST["rnd"].".mp3", "w" );
+    $file = fopen( "test.mp3", "w" );
     fwrite( $file, base64_decode( $_POST["file"] ) );
     fclose($file);
+
+    $result = array('uid' => $_POST["uid"], 'sign'=>$_POST["sign"]);
+    echo json_encode($result);
 
 ?>
